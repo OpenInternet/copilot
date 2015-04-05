debce72e5a60d1d7bee4d3591ea21c37dbf03f6f
Seamus Tuohy <stuohy@internews.org>
2015-04-05 15:31:04 -0400

    continued tweaking of html.

--- copilot/views/config.py
+++ copilot/views/config.py
@@ -87,6 +87,7 @@ def config():
         return redirect(url_for('index'))
     else:
         print("DID NOT VALIDATE")
+        print("ERRORS: {0}".format(form.errors))
     status_items = get_status_items()
     buttons = [{"name":"Submit", "submit":True}]
     return render_template('config.html', form=form, trainer_exists=trainer_exists, status_items=status_items, buttons=buttons)
