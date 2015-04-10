ba8ddfd72cf5e54fce0f71e1b53550d5b6d88a4f
seamus tuohy <stuohy@internews.org>
2015-04-10 11:45:05 -0400

    adding render for blockpage.

--- blockpage/views.py
+++ blockpage/views.py
@@ -8,3 +8,6 @@ def catch_all(path):
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
     image = random.choice(os.listdir(os.path.join(BASE_DIR, "/static/images/dns/")))
     print(image)
+    print("IT GOT HERE")
+    return render_template('blockpage.html', image=image)
+    
