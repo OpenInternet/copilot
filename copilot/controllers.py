43c6c4fee8dac06c669be1beb84cdc05b360eafa
seamus tuohy <stuohy@internews.org>
2015-04-12 17:02:31 -0400

    working on applying profiles

--- copilot/controllers.py
+++ copilot/controllers.py
@@ -22,20 +22,46 @@ def get_status_items():
     status: [off/on/error] The color of the icon background to use (off=grey, on=green, error=orange)
     """
     print("TODO: get_status_items is currently not implemented")
+    profile = get_profile_status()
+    access_point = get_ap_status()
     status_items = [{"icon":"wifi",
-                     "value":"[AP NAME HERE]",
-                     "status":"on",
+                     "value":access_point['value'],
+                     "status":access_point['status'],
                      "url":"config"},
                     {"icon":"config",
                      "value":"Configure",
                      "status":"off",
                      "url":"config"},
                     {"icon":"profile",
-                     "value":"[PROFILE NAME]",
-                     "status":"off",
+                     "value":profile['value'],
+                     "status":profile['status'],
                      "url":"profile_current"},
                     {"icon":"load",
                      "value":"Load Profile",
                      "status":"off",
                      "url":"profile_load"}]
     return status_items
+
+def get_profile_status():
+    trainer = get_trainer()
+    profile = {}
+    current_profile = trainer.current
+    if current_profile:
+        profile['status'] = "on"
+        profile['value'] = current_profile
+    else:
+        profile['status'] = "off"
+        profile['value'] = "NONE"
+    return profile
+
+def get_ap_status():
+    trainer = get_trainer()
+    ap = {}
+    current_ap = trainer.ap_name
+    if current_profile:
+        ap['status'] = "on"
+        ap['value'] = current_ap
+    else:
+        ap['status'] = "off"
+        ap['value'] = "NONE"
+    return ap
