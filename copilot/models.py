bd86f4fae34ab8b7cb145a873ece4253f4193527
seamus tuohy <stuohy@internews.org>
2015-04-21 13:51:16 -0400

    added logging to identify edge case.

--- copilot/models.py
+++ copilot/models.py
@@ -181,9 +181,12 @@ class Profile:
             for rule in self.rules:
                 dnsc_rule = rule.get_dns()
                 if dnsc_rule:
+                    log.debug("Applying DNS rule {0}".format(dnsc_rule))
                     config_file.write(dnsc_rule)
                     config_file.write("=192.168.12.1")
                     config_file.write("\n")
+                else:
+                    log.debug("no DNS rule to apply.")
         log.info("restarting DNSChef")
         subprocess.call(["service", "dnschef", "restart"], shell=True)
 
@@ -207,10 +210,13 @@ class Rule:
     def get_dns(self):
         tld = ["ac", "ad", "ae", "aero", "af", "ag", "ai", "al", "am", "an", "ao", "aq", "ar", "arpa", "as", "asia", "at", "au", "aw", "ax", "az", "ba", "bb", "bd", "be", "bf", "bg", "bh", "bi", "biz", "bj", "bl", "bm", "bn", "bo", "bq", "br", "bs", "bt", "bv", "bw", "by", "bz", "ca", "cat", "cc", "cd", "cf", "cg", "ch", "ci", "ck", "cl", "cm", "cn", "co", "com", "coop", "cr", "cs", "cu", "cv", "cw", "cx", "cy", "cz", "dd", "de", "dj", "dk", "dm", "do", "dz", "ec", "edu", "ee", "eg", "eh", "er", "es", "et", "eu", "fi", "fj", "fk", "fm", "fo", "fr", "ga", "gb", "gd", "ge", "gf", "gg", "gh", "gi", "gl", "gm", "gn", "gov", "gp", "gq", "gr", "gs", "gt", "gu", "gw", "gy", "hk", "hm", "hn", "hr", "ht", "hu", "id", "ie", "il", "im", "in", "info", "int", "io", "iq", "ir", "is", "it", "je", "jm", "jo", "jobs", "jp", "ke", "kg", "kh", "ki", "km", "kn", "kp", "kr", "kw", "ky", "kz", "la", "lb", "lc", "li", "lk", "local", "lr", "ls", "lt", "lu", "lv", "ly", "ma", "mc", "md", "me", "mf", "mg", "mh", "mil", "mk", "ml", "mm", "mn", "mo", "mobi", "mp", "mq", "mr", "ms", "mt", "mu", "museum", "mv", "mw", "mx", "my", "mz", "na", "name", "nato", "nc", "ne", "net", "nf", "ng", "ni", "nl", "no", "np", "nr", "nu", "nz", "om", "onion", "org", "pa", "pe", "pf", "pg", "ph", "pk", "pl", "pm", "pn", "pr", "pro", "ps", "pt", "pw", "py", "qa", "re", "ro", "rs", "ru", "rw", "sa", "sb", "sc", "sd", "se", "sg", "sh", "si", "sj", "sk", "sl", "sm", "sn", "so", "sr", "ss", "st", "su", "sv", "sx", "sy", "sz", "tc", "td", "tel", "tf", "tg", "th", "tj", "tk", "tl", "tm", "tn", "to", "tp", "tr", "travel", "tt", "tv", "tw", "tz", "ua", "ug", "uk", "um", "us", "uy", "uz", "va", "vc", "ve", "vg", "vi", "vn", "vu", "wf", "ws", "xxx", "ye", "yt", "yu", "za", "zm", "zr", "zw"]
         _sub = self.sub_target
+        log.debug("sub target is {0}".format(_sub))
         if _sub == "":
             return None
-        parsed = urlparse(_sub)
+            parsed = urlparse(_sub)
+        log.debug("parsed url is {0}".format(parsed))
         split_sub = string.split(parsed.path, ".")
+        log.debug("split up sub target is {0}".format(split_sub))
         #TODO the below is a monstrosity
         if len(split_sub) > 3:
             raise ValueError("invalid url")
