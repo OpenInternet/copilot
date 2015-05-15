e3b3cca63c419c04c9389e430c503a492c6a5c3c
seamus tuohy <stuohy@internews.org>
2015-05-15 15:45:50 -0400

    continued debugging

--- copilot/utils/plugin.py
+++ copilot/utils/plugin.py
@@ -29,8 +29,8 @@ class Plugin(object):
         # initialize the plugin.
         for plugin_name in self.source.list_plugins():
             log.debug("Loading sub-plugin {0} from plugin: {1}".format(plugin_name, self.name))
-            plugin = self.source.load_plugin("config")
-            self.writer = plugin.setup(self)
+            plugin = self.source.load_plugin(plugin_name)
+            plugin.setup(self)
 
     def get_config_writer(self, plugin_writer):
         self.writer = plugin_writer()
