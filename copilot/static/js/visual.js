ca3f354977b82dc18ef6fea4a8c5d4dea0a2a9ae
seamus tuohy <s2e@seamustuohy.com>
2016-02-24 12:23:00 -0500

    Updated config page

--- copilot/static/js/visual.js
+++
@@ -1,5 +0,0 @@
-//Cause the overlay to appear
-function overlay() {
-        el = document.getElementById("overlay");
-        el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
-}
